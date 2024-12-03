01ef8124cf19dc71c600203902310333a9a2f701
crawlic-stud <nikitosik0726@gmail.com>
2024-12-03 23:20:52 +0300

    change pipeline

--- pipelight.ts
+++ pipelight.ts
@@ -15,7 +15,7 @@ const my_pipe: Pipeline = {
     },
     {
       name: "Run tests",
-      commands: [`docker run ${CONTAINER_NAME} --entrypoint "pytest"`],
+      commands: [`docker run --entrypoint "python -m pytest" ${CONTAINER_NAME}`],
     }
   ],
   triggers: [{
